3af511c9b8e0ea134c33156d45338dc420b3c7c4
AneoX <aneox.inbox@gmail.com>
2020-10-07 18:15:59 +0600

    debug release output files

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -4,24 +4,26 @@ macro(utils_target_generate_hex TARGET_NAME)
         COMMAND
             ${CMAKE_OBJCOPY} -O ihex
                 $<TARGET_FILE:${TARGET_NAME}>
-                ${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}.hex
+                ${CMAKE_CURRENT_BINARY_DIR}/output/${OUTPUTFILENAME}.hex
     )
     add_dependencies(${TARGET_NAME}-hex
         ${TARGET_NAME}
     )
 endmacro()
 
-macro(utils_target_generate_bin TARGET_NAME)
+macro(utils_target_generate_bin TARGET_NAME)   
     add_custom_target(${TARGET_NAME}-bin
         ALL
         COMMAND
             ${CMAKE_OBJCOPY} -O binary
                 $<TARGET_FILE:${TARGET_NAME}>
-                ${CMAKE_CURRENT_BINARY_DIR}/${TARGET_NAME}.bin
+                ${CMAKE_CURRENT_BINARY_DIR}/output/${OUTPUTFILENAME}.bin
     )
-    add_dependencies(${TARGET_NAME}-hex
+    add_dependencies(${TARGET_NAME}-bin
         ${TARGET_NAME}
     )
+
+    set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES "${CMAKE_CURRENT_BINARY_DIR}/output/${OUTPUTFILENAME}.bin")
 endmacro()
 
 macro(utils_target_print_size TARGET_NAME)
@@ -31,7 +33,6 @@ macro(utils_target_print_size TARGET_NAME)
                 ${CMAKE_SIZE_UTIL} --radix=10 $<TARGET_FILE:${TARGET_NAME}>
     )
 endmacro()
-
 
 macro(utils_target_set_linker_script TARGET_NAME LINKER_SCRIPT)
     target_link_options(${TARGET_NAME}
